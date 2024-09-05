3fd54b6a32b9b1326051ca90dc6aa9af96b44df7
Francisco2301 <jlopezm@uamv.edu.ni>
2024-09-05 09:33:59 -0600

    Ejercicios

---
+++ src/semana_2/sesion_martes/models/Animal.java
@@ -0,0 +1,8 @@
+package semana_2.sesion_martes.models;
+
+public class Animal {
+    public void hacer_sonido(){
+        System.out.println("Sonido generico");
+    }
+}
+
---
+++ src/semana_2/sesion_martes/models/Curso.java
@@ -0,0 +1,15 @@
+package semana_2.sesion_martes.models;
+
+public class Curso {
+    private String nombre;
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+    }
+    public Curso(){
+
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+}
